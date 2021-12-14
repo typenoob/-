8977ae1285972f2e4e72bfd23e75603a60642534
zxq4869 <710228552@qq.com>
2021-12-14 11:41:11 +0800

    shape

---
+++ Shape.py
@@ -0,0 +1,70 @@
+
+import tkinter as tk
+from tkinter.simpledialog import *
+
+
+class Shape:
+    def __init__(self, thick):
+        self.thick = thick
+        self.x = 0
+        self.y = 0
+        self.fgcolor = "black"
+        self.lastDraw = 0
+        self.startDrawFlag = False
+        self.drawpad = canvas
+        self.drawpad.pack()
+
+    def stopDraw(self, event):
+        self.startDrawFlag = False
+        self.lastDraw = 0
+
+    def startDraw(self, event):
+        self.drawpad.delete(self.lastDraw)
+        if not self.startDrawFlag:
+            self.startDrawFlag = True
+            self.x = event.x
+            self.y = event.y
+
+    def myLine(self, event):
+        self.startDraw(event)  # 这个设置使得每次画线不会重复出现
+        self.lastDraw = self.drawpad.create_line(self.x, self.y, event.x, event.y,
+                                                 fill=self.fgcolor, width=self.thick)
+
+    def myLineArrow(self, event):
+        self.startDraw(event)
+        self.lastDraw = self.drawpad.create_line(self.x, self.y, event.x, event.y, arrow=LAST,
+                                                 fill=self.fgcolor, width=self.thick)
+
+    def myRect(self, event):
+        self.startDraw(event)
+        self.lastDraw = self.drawpad.create_rectangle(self.x, self.y, event.x, event.y,
+                                                      outline=self.fgcolor, width=self.thick)
+
+    def myOval(self, event):
+        self.startDraw(event)
+        self.lastDraw = self.drawpad.create_oval(self.x, self.y, event.x, event.y,
+                                                 outline=self.fgcolor, width=self.thick)
+
+    def bind(self, canvas, choice):
+        if choice == 1:
+            canvas.bind("<B1-Motion>", self.myLine)
+        elif choice == 2:
+            canvas.bind("<B1-Motion>", self.myLineArrow)
+        elif choice == 3:
+            canvas.bind("<B1-Motion>", self.myRect)
+        elif choice == 4:
+            canvas.bind("<B1-Motion>", self.myOval)
+        canvas.bind("<ButtonRelease-1>", self.stopDraw)
+
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.geometry("1024x648")
+    window.title('shape')
+    window.configure(bg='white')
+    canvas = tk.Canvas(window, width=1000, height=1000,
+                       highlightthickness=0, bg='#AFEEEE')
+    canvas.pack()
+    Shape(3).bind(canvas, 3)
+    window.mainloop()
+
